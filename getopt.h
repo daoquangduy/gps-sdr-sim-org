bf0c7f899aaf11b09fd0af0ffcb46c166fa7d2b5
Dao Quang Duy <dq-duy@mail.peritec.co.jp>
2023-08-21 14:39:39 +0700

    1st commit

---
+++ getopt.h
@@ -0,0 +1,10 @@
+#ifndef GETOPT_H
+#define GETOPT_H
+
+extern char *optarg;
+extern int optind;
+
+int getopt(int nargc, char * const nargv[], const char *ostr) ;
+void reset_getopt(void);
+
+#endif
